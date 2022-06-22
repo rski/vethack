4addc704002b57b0394a5d8a906e56eeb55d003a
Romanos Skiadas <rski@arista.com>
2022-06-22 03:48:13 +0300

    fix docstring

--- vethack.go
+++ vethack.go
@@ -4,8 +4,8 @@ import (
 	"unsafe"
 )
 
-// DoIntoUnsafePtr takes a function, typically a syscall,
-// that returns a uintptr and converts it into an unsafe.Pointer.
+// IntoUnsafePointer takes a uintptr and converts it into an unsafe.Pointer,
+// which is really bad and something you typically absolutely must not do.
 func IntoUnsafePointer(v uintptr) unsafe.Pointer {
 	return unsafe.Pointer(v)
 }
